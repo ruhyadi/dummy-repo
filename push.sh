dbbebdfc186ff47203413086c07e65c40fcebe27
William Justin <williamjustinxu@gmail.com>
2022-05-11 20:13:59 +0700

    update

--- push.sh
+++ push.sh
@@ -10,8 +10,10 @@ git push origin --delete $branch
 git checkout -b $branch
 mkdir annotation
 echo 'Lorem Ipsum' > annotation/$branch.zip
-echo {\"version\": \"$version\"} > package.json
-git add annotation package.json
+# echo {\"version\": \"$version\"} > package.json
+echo $version > version.txt
+# git add annotation package.json
+git add annotation version.txt
 git commit -m "CVAT commit"
 git checkout master
 git push -u origin $branch
